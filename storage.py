103e2402f5b3d0ca168c5e34e79e0356f74e718c
janimmelderim-svg <janim.melderim@gmail.com>
2026-03-11 12:05:51 +0200

    feat: add storage module with load/save expenses

---
+++ storage.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+data_file=expenses.json
+
+def load_expenses():
+    """ielādē izdevumus no json faila"""
+    if not os.path.exists(data_file):
+        return []
+    
+    try:
+        with open(data_file, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return []
+
+def save_expenses(expenses):
+    """saglabā izdevumus json failā"""
+    try:
+        with open(data_file, "w", encoding="utf-8") as f:
+            json.dump(expenses, f, ensure_ascii=False, indent=2)
+        return True
+    except Exception as e:
+        return False
+    
+
+
